602a0e921a8ad538a710417918105a49de11a279
scivision <scivision@users.noreply.github.com>
2025-02-25 12:12:22 -0500

    buildtool: generic func name

--- buildfile.m
+++ buildfile.m
@@ -7,9 +7,10 @@ plan.DefaultTasks = "test";
 pkg_name = "+matmap3d";
 
 if isMATLABReleaseOlderThan("R2023b")
-  plan("test") = matlab.buildtool.Task(Actions=@legacyTestTask);
+  plan("test") = matlab.buildtool.Task(Actions=@legacy_test);
 else
-  plan("check") = matlab.buildtool.tasks.CodeIssuesTask(pkg_name, IncludeSubfolders=true);
+  plan("check") = matlab.buildtool.tasks.CodeIssuesTask(pkg_name, IncludeSubfolders=true, ...
+    WarningThreshold=0);
   plan("test") = matlab.buildtool.tasks.TestTask("test", Strict=false);
 end
 
@@ -17,13 +18,13 @@ if ~isMATLABReleaseOlderThan("R2024a")
   plan("coverage") = matlab.buildtool.tasks.TestTask(Description="code coverage", SourceFiles="test", Strict=false, CodeCoverageResults="code-coverage.xml");
 end
 
-plan("publish") = matlab.buildtool.Task(Description="HTML inline doc generate", Actions=@publishTask);
+plan("publish") = matlab.buildtool.Task(Description="HTML inline doc generate", Actions=@publish_html);
 
 
 end
 
 
-function legacyTestTask(context)
+function legacy_test(context)
 r = runtests(fullfile(context.Plan.RootFolder, "test"), Strict=false);
 % Parallel Computing Toolbox takes more time to startup than is worth it for this task
 
@@ -32,7 +33,7 @@ assertSuccess(r)
 end
 
 
-function publishTask(context)
+function publish_html(context)
   outdir = fullfile(context.Plan.RootFolder, "docs");
 
   publish_gen_index_html("matmap3d", ...
